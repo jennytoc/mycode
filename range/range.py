ea77dcf9228a91874780fd6a487cefbe5093905f
jennytoc <toctocanj@gmail.com>
2022-11-10 22:33:25 +0000

    range() practice

---
+++ range/range.py
@@ -0,0 +1,4 @@
+def main():
+    for i in range(99, 0, -1):
+        print(f"{i} bottles of beer on the wall! \n {i} bottles of beer on the wall! {i} bottles of beer! You take one down, pass it around!")
+main()
